2e4f9e29558370b9302d6da6c8dd49af91151767
Katy Long <katy.long@wearecodenation.com>
2024-11-26 16:14:26 +0000

    notes started

--- line_graph.py
+++ line_graph.py
@@ -1,5 +1,28 @@
-- Make a new folder in your data course folder called plot_examples
 
-- make a new file in that folder called line_graph.py
+#* ---------- Matplotlib ----------------------------
 
-- pop a thumbs up in that chat when you're good to see github'
+#? we import the pyplot module from matplotlib, and refer to it as plt in our code.
+#? This is a common alias, most things will say "as plt"
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+years = [2018, 2019, 2020, 2021, 2022, 2023]
+
+python_position = [7, 4, 4, 3, 4, 3]
+javascript_position = [1, 1, 1, 1, 1, 1]
+sql_position = [4, 3, 3, 4, 3, 4]
+typescript_position = [12, 10, 9, 7, 5, 5]
+
+plt.plot(years, python_position, label = "Python")
+plt.plot(years, javascript_position, label = "JavaScript", linestyle = "dashed")
+plt.plot(years, sql_position, label = "SQL", linestyle = "dotted")
+plt.plot(years, typescript_position, label = "TypeScript", linestyle="dashdot")
+plt.title("Python Rankings in Stack Overflow Survey")
+plt.xlabel("Year")
+plt.ylabel("Position in Survey")
+plt.ylim(bottom=13, top = 0)
+
+# plt.legend(["Python", "JavaScript", "SQL", "TypeScript"])
+plt.legend()
+plt.show()
