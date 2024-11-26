3fba9f0bc72a1f27786a151da67f494dee38f892
Katy Long <katy.long@wearecodenation.com>
2024-11-26 09:40:31 +0000

    first commit

---
+++ line_graph.py
@@ -0,0 +1,5 @@
+- Make a new folder in your data course folder called plot_examples
+
+- make a new file in that folder called line_graph.py
+
+- pop a thumbs up in that chat when you're good to see github'
