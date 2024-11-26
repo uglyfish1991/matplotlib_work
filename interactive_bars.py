a592ed62ade32fac27aecc32dde7e2b766ec61d7
Katy Long <katy.long@wearecodenation.com>
2024-11-26 17:12:19 +0000

    notes added

---
+++ interactive_bars.py
@@ -0,0 +1,23 @@
+# import matplotlib.pyplot as plt
+# import mplcursors
+
+# # Sample data
+# categories = ['A', 'B', 'C', 'D']
+# values = [10, 20, 15, 30]
+
+# fig, ax = plt.subplots()
+# bars = ax.bar(categories, values)
+
+# # Add cursor for interactivity
+# mplcursors.cursor(bars, hover=True)
+
+# plt.show()
+
+import plotly.express as px
+
+# Sample data
+data = {'Category': ['A', 'B', 'C', 'D'], 'Value': [10, 20, 15, 30]}
+fig = px.bar(data, x='Category', y='Value', text='Value')
+
+fig.update_traces(texttemplate='%{text}', textposition='outside')
+fig.show()
--- line_graph.py
+++ line_graph.py
@@ -5,24 +5,49 @@
 #? This is a common alias, most things will say "as plt"
 import matplotlib.pyplot as plt
 
+#? optional - matplotlib can have stylesheets applied to it to add formatting to a plot all in one go. The most used is ggplot, though there are many others:
+#? https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html 
+
 plt.style.use('ggplot')
 
+#? Our aim is to visualise some of the information we took from the staff overflow developer survey. 
+#? As these are changes over time, a line graph would be a good plot to produce. 
+
+#? We can store the different axis information in any kind of collection - for now we'll use a list. 
+
+#? The years we are tracking - these will make up our x-axis
 years = [2018, 2019, 2020, 2021, 2022, 2023]
 
+#? The languages we are tracking. We can plot all of these on the same graph to help compare them. 
 python_position = [7, 4, 4, 3, 4, 3]
 javascript_position = [1, 1, 1, 1, 1, 1]
 sql_position = [4, 3, 3, 4, 3, 4]
 typescript_position = [12, 10, 9, 7, 5, 5]
 
+#? When we create a plot, Matplotlib generates a "figure" object. 
+#? A figure acts as the container for all elements of the plot (axes, labels, titles, etc.). 
+#? By default, all plots are added to the current active figure unless a new figure is explicitly created.
+
+#? Some of these plots have been customised - we've added labels to them to help with creating a legend, and some plots have linestyles on them
+#? This can help identify the lines when colour can't be relied on. 
+
 plt.plot(years, python_position, label = "Python")
 plt.plot(years, javascript_position, label = "JavaScript", linestyle = "dashed")
 plt.plot(years, sql_position, label = "SQL", linestyle = "dotted")
 plt.plot(years, typescript_position, label = "TypeScript", linestyle="dashdot")
+
+
+#? We can use these methods to add features to the active figure. They are well named and readable!
+
 plt.title("Python Rankings in Stack Overflow Survey")
 plt.xlabel("Year")
 plt.ylabel("Position in Survey")
+
+#? Unusually, because of the theme of our plot, we need to flip our y axis so the largest number is at the bottom. This will show languages moving up or down the rankings in the orientation we expect!
 plt.ylim(bottom=13, top = 0)
 
-# plt.legend(["Python", "JavaScript", "SQL", "TypeScript"])
+#? Because we have many observations on one plot, a legend can help identify the different lines used on the plot. Without it, we wouldn't know which line was which!
 plt.legend()
+
+#? The last thing you do is plt.show() - this instructs matplotlib to follow your instructions and draw the chart, and then show you the result. After it has done this, it wipes the chart! 
 plt.show()
